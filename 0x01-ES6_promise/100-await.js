47e1886c436c650b7c2b00d8f2890190d7cb370d
Oussama-hamdi <ohamdi776@gmail.com>
2024-01-05 14:42:26 +0000

    Fixed task 10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   try {
     const [photoRes, userRes] = await Promise.all([uploadPhoto(), createUser()]);
-    return { photo: photoRes, userRes};
+    return { photo: photoRes, userRes: user };
   } catch (err) {
     return { photo: null, user: null };
   }
