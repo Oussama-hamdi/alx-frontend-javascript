dc21716ca4a42d56d78992caeb75e0f69736460d
Oussama-hamdi <ohamdi776@gmail.com>
2024-01-08 15:52:43 +0000

    fixed task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
-
-  const strArr = [];
-
-  set.forEach((str) => {
-    if (str.startsWith(startString)) {
-      strArr.push(str.slice(startString.length));
-    }
-  });
-
-  return strArr.join('-');
+  if (startString === '') {
+    return [].join('-');
+  }
+  return [...set]
+    .filter((val) => val.startsWith(startString))
+    .map((val) => val.slice(startString.length)).join('-');
 }
